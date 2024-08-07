a474e41c1f175ce36a64173961c3cbdecf452eca
Jáderson Válner Ferreira de Lima <160531175+jaderson13@users.noreply.github.com>
2024-08-07 08:43:23 -0300

    Delete projetos/jogo_da_forca directory

--- projetos/jogo_da_forca/script.js
+++
@@ -1,8 +0,0 @@
-onload = () => {
-    //Tela_inicial();
-}
-
-function Tela_inicial() {
-    document.querySelector(".tela_inicial").innerHTML =
-    `<h1>Jogo da forca</h1> <h2>Iniciar</h2> <button id="btn_iniciar"><i class="fa-solid fa-play"></i></button>`;
-}
